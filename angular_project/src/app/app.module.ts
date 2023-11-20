1ada646e89b3d51a94111894a4bd7bf38441dd45
Chiara Pala <chiara.pala99.cp@.com>
2023-11-20 18:55:11 +0100

    Dashboard application

--- angular_project/src/app/app.module.ts
+++ angular_project/src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Pagina1Component } from './components/pagina1/pagina1.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
+import { Pagina3Component } from './components/pagina3/pagina3.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular_project/src/app/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+//     return true;
+//   }
+  
+// }
+
+ export const AuthGuard: CanActivateFn = () => {
+  console.log(inject(AuthService).isLoggedIn)
+  return inject(AuthService).isLoggedIn;
+} 
